6a7e8b07a28c7248d545e1644b601328b95141dd
Batyrbek <kasimov.batyr@gmail.com>
2024-04-13 13:55:10 +0300

    Test

--- src/components/MyHello.vue
+++ src/components/MyHello.vue
@@ -2,15 +2,13 @@
 import { ref } from 'vue'
 
 const count = ref(0)
-const increment = () => {
-  count.value++
-}
+const increment = () => count.value++
 </script>
 
 <template>
   <h1>{{ count }}</h1>
   <button @click="increment">+</button>
-  <h1>Hello</h1>
+  <input v-model="count" />
 </template>
 
 <style scoped></style>
